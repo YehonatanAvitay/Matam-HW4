1d2e8bf0c1e50c91698b079c366734055cd7650c
root <root@LAPTOP-TIKRIBLN>
2024-08-14 18:16:29 +0300

    Event Factory

--- Events/EventFactory.h
+++ Events/EventFactory.h
@@ -5,8 +5,18 @@
 #include "Event.h"
 #include "SpecialEvent.h"
 #include "Encounter.h"
+#include <sstream>
+#include <iostream>
+#include <unordered_map>
+#include <functional>
 
 class EventFactory {
+    static std::unordered_map<string,std::function<unique_ptr<Event>(const vector<string>&)>>
+    eventsFactoriesMap;
+    static vector<string> splitString(const string&);
+    static unique_ptr<Pack> createPackRecursively(std::vector<string>::iterator it);
+    static bool isInteger(const string&);
 public:
-    unique_ptr<Event> createEvent();
+    static unique_ptr<Pack> createPack(const vector<string>&);
+    vector<unique_ptr<Event>> createEventList(std::istream& Eventsfile);
 };

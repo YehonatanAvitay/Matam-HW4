69b902a21b2f3a5fb8d36632138357b140fe72e8
unknown <shaked614@gmail.com>
2024-08-15 13:43:57 +0300

    ReImplemented PLayersFactory to use map. Character and Job factories included in PLayerFactory class

--- Players/PlayersFactory.cpp
+++ Players/PlayersFactory.cpp
@@ -1,92 +1,79 @@
-#include "PlayersFactory.h"
+#pragma once
+
 #include <unordered_map>
-#include "../Events/Event.h"
-#include "../Events/Encounter.h"
-#include "../Events/SpecialEvent.h"
+#include <functional>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#include "PlayersFactory.h"
 #include "Player.h"
+#include "Character.h"
+#include "Job.h"
 
-std::vector<std::unique_ptr<Player>> PlayersFactory::readPlayers(std::istream& input) {
+ std::vector<std::unique_ptr<Player>> PlayersFactory::readPlayers(std::istream& input) {
     std::vector<std::unique_ptr<Player>> players;
-    string line;
-
+    std::string line;
     int playersCounter = 0;
+
     while (std::getline(input, line)) {
         std::istringstream lineStream(line);
-
-        // Create a new Player and add it to the vector
-        players.push_back(createPlayer(lineStream));//std::make_unique<Player>(name, std::move(job), std::move(character)));
-
+        players.push_back(createPlayer(lineStream));
         playersCounter++;
     }
 
-    // Throw exception if number of players is not in the defined range
     if (playersCounter < 2 || playersCounter > 6) {
-        throw std::runtime_error("Invalid player file");
+        throw std::runtime_error("Invalid Players File");
     }
     return players;
 }
 
-std::unique_ptr<Player> PlayersFactory::createPlayer(std::istringstream& lineStream) {
 
-    string name, jobName, characterName;
+std::unique_ptr<Player> PlayersFactory::createPlayer(std::istringstream& lineStream) {
+    std::string name, jobName, characterName;
 
     if (!(lineStream >> name >> jobName >> characterName)) {
-        throw std::runtime_error("Invalid player file");
+        throw std::runtime_error("Invalid Players File");
     }
 
-    // Throw exception if there are more data in line
-    string extra;
+    std::string extra;
     if (lineStream >> extra) {
-        throw std::runtime_error("Invalid player file");
+        throw std::runtime_error("Invalid Players File");
     }
 
-    JobFactory jobFactory;
-    CharacterFactory characterFactory;
+    auto job = createJob(jobName);
+    auto character = createCharacter(characterName);
 
-    // Create Job object based on the input
-    std::unique_ptr<Job> job;
-    if (jobName == "Warrior") {
-        job = jobFactory.createWarrior();
-    } else if (jobName == "Magician") {
-        job = jobFactory.createMagician();
-    } else if (jobName == "Archer") {
-        job = jobFactory.createArcher();
-    } else {
-        throw std::runtime_error("Invalid player file");
+    return std::make_unique<Player>(name, std::move(job), std::move(character));
+}
+
+std::unique_ptr<Job> PlayersFactory::createJob(const std::string& jobName) {
+    auto it = jobFactoriesMap.find(jobName);
+    if (it == jobFactoriesMap.end()) {
+        throw std::runtime_error("Invalid Players File");
     }
+    return it->second();
+}
 
-    // Create Character object based on the input
-    std::unique_ptr<Character> character;
-    if (characterName == "RiskTaking") {
-        character = characterFactory.createRiskTaking();
-    } else if (characterName == "Responsible") {
-        character = characterFactory.createResponsible();
-    } else {
-        throw std::runtime_error("Invalid player file");
+std::unique_ptr<Character> PlayersFactory::createCharacter(const std::string& characterName) {
+    auto it = characterFactoriesMap.find(characterName);
+    if (it == characterFactoriesMap.end()) {
+        throw std::runtime_error("Invalid Players File");
     }
-
-    return std::make_unique<Player>(name, std::move(job), std::move(character);
-
+    return it->second();
 }
 
-PlayersFactory::characterCreators.
+const std::unordered_map<std::string, std::function<std::unique_ptr<Job>()>>
+        PlayersFactory::jobFactoriesMap = {
+        {"Warrior", []() { return std::make_unique<Warrior>(); }},
+        {"Archer", []() { return std::make_unique<Archer>(); }},
+        {"Magician", []() { return std::make_unique<Magician>(); }}
+};
 
-unique_ptr<Warrior> PlayersFactory::JobFactory::createWarrior() {
-    return std::make_unique<Warrior>();
-}
-
-unique_ptr<Archer> PlayersFactory::JobFactory::createArcher() {
-    return std::make_unique<Archer>();
-}
-
-unique_ptr<Magician> PlayersFactory::JobFactory::createMagician() {
-    return std::make_unique<Magician>();
-}
-
-unique_ptr<RiskTaking> PlayersFactory::CharacterFactory::createRiskTaking() {
-    return std::make_unique<RiskTaking>();
-}
-
-unique_ptr<Responsible> PlayersFactory::CharacterFactory::createResponsible() {
-    return std::make_unique<Responsible>();
-}
+const std::unordered_map<std::string, std::function<std::unique_ptr<Character>()>>
+        PlayersFactory::characterFactoriesMap = {
+        {"RiskTaking", []() { return std::make_unique<RiskTaking>(); }},
+        {"Responsible", []() { return std::make_unique<Responsible>(); }}
+};
--- Players/PlayersFactory.h
+++ Players/PlayersFactory.h
@@ -1,44 +1,34 @@
 #pragma once
 
-#include <vector>
+#include <unordered_map>
+#include <functional>
 #include <memory>
+#include <string>
 #include <sstream>
-#include <map>
-#include "Player.h"
-#include "Job.h"
-#include "Character.h"
+#include <stdexcept>
+#include <vector>
 
+//class Player;
+//class Job;
+//class Character;
+//class Warrior;
+//class Archer;
+//class Magician;
+//class RiskTaking;
+//class Responsible;
 
 class PlayersFactory {
-    class JobFactory;
-    class CharacterFactory;
-public:
-    std::vector<std::unique_ptr<Player>> readPlayers(std::istream& input);
-    std::unique_ptr<Player> createPlayer(std::istringstream& lineStream);
-
-
-};
-
-class PlayersFactory::JobFactory {
-public:
-    // Functions to emulate constructors of jobs
-    unique_ptr<Warrior> createWarrior();
-    unique_ptr<Archer> createArcher();
-    unique_ptr<Magician> createMagician();
-};
-
-class PlayersFactory::CharacterFactory {
-    // Map to store character creation functions
-    std::map<std::string, std::unique_ptr<Character>(*)()> characterCreators;
+private:
+    static const std::unordered_map<std::string, std::function<std::unique_ptr<Job>()>> jobFactoriesMap;
+    static const std::unordered_map<std::string, std::function<std::unique_ptr<Character>()>> characterFactoriesMap;
 
 public:
-    CharacterFactory() {
-        characterCreators["RiskTaking"] = &createRiskTaking;
-        characterCreators["Responsible"] = &createResponsible;
-    }
+    static std::vector<std::unique_ptr<Player>> readPlayers(std::istream& input);
 
-    // Function prototypes for character creation
-    std::unique_ptr<Character> createRiskTaking();
-    std::unique_ptr<Character> createResponsible();
-    ;
+private:
+    static std::unique_ptr<Player> createPlayer(std::istringstream& lineStream);
+
+    static std::unique_ptr<Job> createJob(const std::string& jobName);
+
+    static std::unique_ptr<Character> createCharacter(const std::string& characterName);
 };

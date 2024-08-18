7fa51e06243228a6e1ff68a770cc715a5b03dcdf
unknown <shaked614@gmail.com>
2024-08-18 16:53:47 +0300

    Document Player class and functions

--- Players/Player.cpp
+++ Players/Player.cpp
@@ -1,5 +1,14 @@
 #include "Player.h"
 
+/**
+ * Constructor for Player .
+ *
+ * Initializes a Player object with the given name, job, and character.
+ *
+ * @param name The name of the player.
+ * @param job A unique pointer to the player's job.
+ * @param character A unique pointer to the player's character.
+ */
 Player::Player(string name, unique_ptr<Job> job, unique_ptr<Character> character) :
         name(std::move(name)), level(job->getLevel()), force(job->getForce()),
         coins(job->getCoins()), maxHealthPoints(job->getMaxHealthPoints()),
@@ -7,12 +16,23 @@ Player::Player(string name, unique_ptr<Job> job, unique_ptr<Character> character
         character(std::move(character)) {
 }
 
+/**
+ * Copy constructor for Player.
+ *
+ * @param other The Player object to copy from.
+ */
 Player::Player(const Player& other) : name(other.name), level(other.level), force(other.force),
                                       coins(other.coins), maxHealthPoints(other.maxHealthPoints),
                                       currentHealthPoints(other.currentHealthPoints), job(other.job->clone()),
                                       character(other.character->clone()), isAlive(other.isAlive) {
 }
 
+/**
+ * Assignment operator for Player.
+ *
+ * @param other The Player object to assign from.
+ * @return A reference to this Player object.
+ */
 Player& Player::operator=(const Player& other) {
     name = other.name;
     level = other.level;
@@ -74,6 +94,11 @@ int Player::getCoins() const {
     return coins;
 }
 
+/**
+ * Gets the maximum health points of the player.
+ *
+ * @return The maximum health points of the player.
+ */
 int Player::getMaxHealthPoints() const {
     return maxHealthPoints;
 }
@@ -87,18 +112,38 @@ int Player::getHealthPoints() const {
     return currentHealthPoints;
 }
 
+/**
+ * Checks if the player is still alive.
+ *
+ * @return True if the player is alive, false otherwise.
+ */
 bool Player::isStillPlaying() const {
     return isAlive;
 }
 
+/**
+ * Gets a constant reference to the player's job.
+ *
+ * @return A constant reference to the Job object.
+ */
 const Job& Player::getJob() const {
     return *job;
 }
 
+/**
+ * Gets a constant reference to the player's character.
+ *
+ * @return A constant reference to the Character object.
+ */
 const Character& Player::getCharacter() const {
     return *character;
 }
 
+/**
+ * Sets the player's level.
+ *
+ * @param num The new level of the player. max value at 10.
+ */
 void Player::setLevel(unsigned int num) {
     if (num > 10) {
         num = 10;
@@ -106,6 +151,11 @@ void Player::setLevel(unsigned int num) {
     level = num;
 }
 
+/**
+ * Sets the player's force points.
+ *
+ * @param num The new force points of the player. Minimum value at 0.
+ */
 void Player::setForce(int num) {
     if (num < 0) {
         num = 0;
@@ -113,10 +163,20 @@ void Player::setForce(int num) {
     force = num;
 }
 
+/**
+ * Sets the amount of coins the player has.
+ *
+ * @param num The new amount of coins.
+ */
 void Player::setCoins(unsigned int num) {
     coins = num;
 }
 
+/**
+ * Sets the player's current health points.
+ *
+ * @param num The new amount of health points. Values are capped at 0 and the maximum health points.
+ */
 void Player::setHealthPoints(int num) {
     if (num <= 0) {
         num = 0;
@@ -127,6 +187,15 @@ void Player::setHealthPoints(int num) {
     }
     currentHealthPoints = num;
 }
+
+/**
+ * Comparison operator: compares this Player to another Player
+ * @return true if this Player is considered "less than" the other Player
+ * Criteria for comparison:
+ * -> Higher level is considered greater (i.e., "less than" in reverse)
+ * --> If levels are equal, more coins is considered greater
+ * ----> If levels and coins are equal, the Player with a lexicographically smaller name is considered less
+**/
 
 bool Player::operator<(const Player& player) const {
     if (this->level > player.level) {
